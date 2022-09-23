315a7e1e3e139bc0156071d2c931fd9e553a6a59
Martin <ove.martin.malm@gmail.com>
2022-09-23 19:36:43 +0200

    Added swagger

--- src/main/java/com/microsoft/azure/ManagedIdentityStorageBlobApplication.java
+++ src/main/java/com/microsoft/azure/ManagedIdentityStorageBlobApplication.java
@@ -8,8 +8,9 @@ package com.microsoft.azure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-
+@EnableWebMvc
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ManagedIdentityStorageBlobApplication {
